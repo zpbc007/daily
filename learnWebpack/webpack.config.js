10b571f2b16de6618c6e3c93ceb4772e18a5c601
zhao peng <719197352@qq.com>
2017-11-08 10:38:05 +0800

    开发 express

--- learnWebpack/webpack.config.js
+++ learnWebpack/webpack.config.js
@@ -21,6 +21,8 @@ module.exports = {
     ],
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        // 服务端使用
+        publicPath: '/'
     },
 }

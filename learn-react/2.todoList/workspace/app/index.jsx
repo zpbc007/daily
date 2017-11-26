7efff671bee015528d272751fee8628e0bcd35b2
赵鹏 <zhaopeng@zhaopengdeMacBook-Pro.local>
2017-11-26 14:41:49 +0800

    更改路径

--- learn-react/2.todoList/workspace/app/index.jsx
+++ learn-react/2.todoList/workspace/app/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+// import Hello from './containers/Hello'
+import Todo from 'containers/Todo'
+
 render(
-    </>,
+    <Todo/>,
     document.getElementById('root')
 )

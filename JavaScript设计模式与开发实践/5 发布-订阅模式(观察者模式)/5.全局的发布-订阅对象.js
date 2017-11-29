e2c74e2c0ba93e9f2f2b19d1370c32de727d2aec
赵鹏 <zhaopeng@zhaopengdeMBP.lan>
2017-11-29 23:41:36 +0800

    订阅者

--- JavaScript设计模式与开发实践/5 发布-订阅模式(观察者模式)/5.全局的发布-订阅对象.js
+++ JavaScript设计模式与开发实践/5 发布-订阅模式(观察者模式)/5.全局的发布-订阅对象.js
@@ -10,7 +10,38 @@ var Event = (function () {
         remove
     listen = function (key, fn) {
         if (!clientList[key]) {
-            clientList[]
+            clientList[key] = []
+        }
+        clientList[key].push(fn)
+    }
+
+    trigger = function () {
+        var key = Array.prototype.shift.call(arguments),
+            fns = clientList.key
+        if (!fns || fns.length === 0) {
+            return false
+        }
+        for (var i = 0, fn; fn = fns[i++];) {
+            fn.apply(this, arguments)
         }
     }
+
+    remove = function (key, fn) {
+        var fns = clientList[key]
+        if (!fns) {
+            return false
+        }
+        if (!fns) {
+            fns && (fns.length = 0 )
+        } else {
+            for (var l = fns.length - 1; l > 0; i--) {
+                var _fn = fns[l]
+                if (_fn === fn) {
+                    fns.splice(l, 1)
+                }
+            }
+        }
+    }
+
+    return {listen, trigger, remove}
 })()
